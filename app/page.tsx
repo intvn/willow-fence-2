df5943fbde0b611d45ff3bf8676648fc67956303
EloraP <eloraira@gmail.com>
2024-10-30 17:17:38 +0000

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 // pages/hello.js
+import Header from '../components/Header';
 
 export default function HelloWorld() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Header />
       <h1>Hello Suffolk Willow</h1>
     </div>
   );
