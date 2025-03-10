485e63b78fe75254159fffdce5d64039edb422c5
EloraP <eloraira@gmail.com>
2025-03-10 14:29:05 +0000

    get rid of swear words

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ export default function HelloWorld() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <h1>Hello Suffolk Willow</h1>
-      <p> Hi I'm Intan </p>
-      <p> fuck Intan</p>
     </div>
   );
 }
