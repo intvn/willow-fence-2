6eb5442df4ef48d686b9e5481e34cdcf79fd35e9
EloraP <eloraira@gmail.com>
2025-03-10 15:53:23 +0000

    pages and links

---
+++ components/header/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold hover:underline">Suffolk Willow</Link>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/" className="hover:underline">Home</Link>
+            </li>
+            <li>
+              <Link href="/aboutUs" className="hover:underline">About</Link>
+            </li>
+            <li>
+              <Link href="/contactUs" className="hover:underline">Contact</Link>
+            </li>
+            <li>
+              <Link href="/portfolio" className="hover:underline">Portfolio</Link>
+            </li>
+            <li>
+              <Link href="/reviews" className="hover:underline">Reviews</Link>
+            </li>
+            <li>
+              <Link href="/instagram" className="hover:underline">Instagram</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
