380d71c6a6c3929f1e9fce371ce1c222b83abf48
intvn <tunkuintan12@gmail.com>
2025-03-11 15:57:59 +0800

    fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 // pages/hello.js
-import Header from '../components/Header';
 
 import Header from '../components/header'
 
 export default function Home() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Header />
-      <h1>Hello Suffolk Willow</h1>
+    <div>
+      <Header/>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <h1>Hello Suffolk Willow</h1>
+      </div>
     </div>
     
   );
