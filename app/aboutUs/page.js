95479eb52caaadb4e40586f36e76a72e1215ab8e
intvn <tunkuintan12@gmail.com>
2025-03-17 12:28:38 +0800

    added footer to all pages

--- app/aboutUs/page.js
+++ app/aboutUs/page.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "../../components/header"
+import Footer from "../../components/footer"
+
 
 export default function ContactUs() {
   return (
@@ -8,6 +10,7 @@ export default function ContactUs() {
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
             <h1>About Us</h1>
         </div>
+        <Footer/>
     </div>
     
   );
